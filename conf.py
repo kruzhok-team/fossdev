046f512602ef440658c2d7827ec3b11b3fed12ee
Artem Vesnin <artemvesnin@gmail.com>
2023-03-01 11:36:59 +0000

    Added documentation auto build

---
+++ conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'toolchain'
+copyright = '2023, Artem Vesnin'
+author = 'Artem Vesnin'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['myst_parser']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'ru'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'restructuredtext',
+    '.md': 'markdown',
+}
---
+++ index.rst
@@ -0,0 +1,40 @@
+.. toolchain documentation master file, created by
+   sphinx-quickstart on Wed Mar  1 10:24:16 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to toolchain's documentation!
+=====================================
+
+.. toctree::
+  :maxdepth: 2
+  :caption: Contents:
+
+  00_terms/main.md
+  01_open_source/main.md
+  02_open_license/main.md
+  04_mvp/main.md
+  05_git_base/main.md
+  06_github/main.md
+  09_code_to_lib/main.md
+  10_packages/main.md
+  12_docs/main.md
+  13_uml/main.md
+  14_styles/main.md
+  16_bash/main.md
+  17_path_venv/main.md
+  22_ide/main.md
+  23_testing/main.md
+  24_logging/main.md
+  25_docker_base/main.md
+  31_docker_project/main.md
+  32_docker_network/main.md
+  33_dockerhub/main.md
+  36_github_pages/main.md
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
